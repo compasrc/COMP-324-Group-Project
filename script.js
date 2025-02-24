211a1fafd35a7560d1afc0ffa317259bc4d8bdf0
Syedmoosa12 <fahad_moosa23@yahoo.com>
2025-02-24 11:36:20 -0600

    Adjusted mission statement. Made changes in footer.

--- script.js
+++ script.js
@@ -33,24 +33,4 @@ gsap.from(".box-container", {
   });
 
   
-  // Initialize Swiper
-  const swiper = new Swiper('.swiper-container', {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    breakpoints: {
-        768: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-        },
-        1024: {
-            slidesPerView: 3,
-            spaceBetween: 30,
-        }
-    }
-});
-
   
