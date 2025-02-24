a2df57585f59707af7ca627de94b003a0da68078
Awalwyn <118495079+Awalwyn@users.noreply.github.com>
2025-02-24 15:03:50 -0600

    Added audio features for user typing and burning message.

--- confess.js
+++ confess.js
@@ -11,6 +11,17 @@ document.addEventListener("DOMContentLoaded", function () {
     const counterSpan = document.getElementById("counter");
     // 'counterSpan' references the <span> element inside <div id="confessionCounter">
     // where the counter number is displayed.
+
+    //audio files for website
+    const burnSound = new Audio("fire.mp3");
+    const whisper = new Audio("whisper.mp3");
+    whisper.volume = .3;
+    burnSound.volume = .5;
+  
+    //init for bool to check if user typing and dec. for timer var
+    let istyping = false;
+    let timer;
+
   
     function burnMessage() {
       // This function is called whenever the user clicks the "Burn" button.
@@ -19,14 +30,23 @@ document.addEventListener("DOMContentLoaded", function () {
         alert("Enter something in the textfield");
         return;
       }
+
+      //pause + reset whisper sound
+      whisper.pause();
+      whisper.currentTime = 0;
+      
+      burnSound.play();
       
       // Clear the textarea
-      messageInput.value = "";
+      setTimeout(function() {
+        messageInput.value = "";
+      }, 800);
+      
   
       // 2. Increment the counter
       // First, we get the current counter value as a number (parseInt).
       
-      let currentCount = parseInt(counterSpan.textContent) ;
+      let currentCount = parseInt(counterSpan.textContent);
   
       // Then, we set the text content to be the old count plus 1.
       counterSpan.textContent = currentCount + 1;
@@ -43,5 +63,35 @@ document.addEventListener("DOMContentLoaded", function () {
         burnMessage();
       }
     });
+    
+    //listener for user typing (keydown event)
+    messageInput.addEventListener("keydown", function () {
+      if (!istyping) {
+        istyping = true;
+        whisper.currentTime = 0;
+        whisper.play();
+      }
+
+      //reset stop timer and times out if no typing 
+      clearTimeout(timer);
+      timer = setTimeout(stopWhisper, 500);
+    });
+    
+    //restarts whisper if 'ended' event takes place
+    whisper.addEventListener("ended", function() {
+      if (istyping) {
+        whisper.currentTime = 0;
+        whisper.play();
+      }
+    });
+
+    //function to stop whisper if user is not typing
+    function stopWhisper() {
+      istyping = false;
+      whisper.pause();
+      whisper.currentTime = 0;
+      istyping = false;
+  }
+  
   });
   
